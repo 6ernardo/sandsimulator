da58926e7267598f0b5e71177b0dca6e4d891ec8
Bernardo <6ernardocampos@gmail.com>
2024-12-10 22:33:31 +0000

    Now shows selected button

--- listeners.js
+++ listeners.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('sand_button').addEventListener('click', () => {
         erase = false;
+        document.getElementById('sand_button').className = "selected";
+        document.getElementById('erase_button').className = "";
     });
 
     document.getElementById('erase_button').addEventListener('click', () => {
         erase = true;
+        document.getElementById('sand_button').className = "";
+        document.getElementById('erase_button').className = "selected";
     });
 
     document.getElementById('clear_button').addEventListener('click', () => {
